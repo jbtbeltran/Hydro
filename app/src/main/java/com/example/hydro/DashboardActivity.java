1e94c8826cd51da7843e9c8d8891dcde18f79352
jbtbeltran <94858188+jbtbeltran@users.noreply.github.com>
2022-12-16 06:35:41 +0800

    UI changes

--- app/src/main/java/com/example/hydro/DashboardActivity.java
+++ app/src/main/java/com/example/hydro/DashboardActivity.java
@@ -3,7 +3,9 @@ package com.example.hydro;
 import static android.content.ContentValues.TAG;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,6 +19,10 @@ import com.google.firebase.database.ValueEventListener;
 
 public class DashboardActivity extends BaseActivity {
     TextView textPhLevel;
+    private TextView txtProgress;
+    private ProgressBar progressBar;
+    private int pStatus = 0;
+    private Handler handler = new Handler();
 
 
     @Override
@@ -27,7 +33,8 @@ public class DashboardActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        textPhLevel = findViewById(R.id.textViewPHLv);
+
+        textPhLevel = findViewById(R.id.textViewPHLvLb);
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference ref = database.getReference("/sensors/ph_level");
 
@@ -44,7 +51,34 @@ public class DashboardActivity extends BaseActivity {
             }
         });
 
+        txtProgress = (TextView) findViewById(R.id.textViewPHLv);
+        progressBar = (ProgressBar) findViewById(R.id.progressBarPHLv);
 
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (pStatus <= 100) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setProgress(pStatus);
+                            txtProgress.setText(pStatus + " %");
+                        }
+                    });
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    pStatus++;
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 
 
